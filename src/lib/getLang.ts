9f4ddc80b29a99287ef5f4fa238cfdf45e634df2
Julionate <julionate@proton.me>
2026-02-19 22:48:17 -0300

    fix: nvm I forgot that you cannot interact with document element

--- src/lib/getLang.ts
+++
@@ -1,7 +0,0 @@
-import { type availableLocalesByCode, defaultLocale } from "@/data/locales";
-import { isLang } from "@/lib/isLang";
-
-export const getLangByHTML = (): availableLocalesByCode => {
-	const lang = document.documentElement.lang;
-	return isLang(lang) ? lang : defaultLocale;
-};
